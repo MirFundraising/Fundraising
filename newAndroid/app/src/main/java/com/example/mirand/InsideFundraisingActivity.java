74ab1db80abc5fed7cb82c32520a7287bde66ede
ilya <ivgorbach@edu.hse.ru>
2021-03-11 04:10:14 +0300

    added notification layouts, some rows, inside fundraising layout and ect

--- newAndroid/app/src/main/java/com/example/mirand/InsideFundraisingActivity.java
+++ newAndroid/app/src/main/java/com/example/mirand/InsideFundraisingActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class fundraising_row extends AppCompatActivity {
+public class InsideFundraisingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fundraising_row);
+        setContentView(R.layout.activity_inside_fundraising);
     }
 }
