ce7214e2a09cd387f6e0959e46aca3db5a033a8b
evbaranova-1 <evbaranova-1@edu.hse.ru>
2021-04-01 22:54:35 +0300

    add tests

--- springServer/src/test/java/com/example/demo/serverClasses/FundraisingTest.java
+++ springServer/src/test/java/com/example/demo/serverClasses/FundraisingTest.java
@@ -2,11 +2,19 @@ package com.example.demo.serverClasses;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FundraisingTest {
 
+    long fundraisingId = 666L;
+
+    Fundraising fundraising = new Fundraising(fundraisingId, 0, false, "", Calendar.getInstance(), new ArrayList<>(), 0);
+
     @Test
     void getFundraisingId() {
+        assertEquals(fundraising.getFundraisingId(), fundraisingId);
     }
 }
