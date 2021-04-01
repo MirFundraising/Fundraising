3448acc54f9aac818bfa6e01366382d27740824e
evbaranova-1 <evbaranova-1@edu.hse.ru>
2021-04-01 21:56:14 +0300

    add tests

--- springServer/src/main/java/com/example/demo/serverClasses/Fundraising.java
+++ springServer/src/main/java/com/example/demo/serverClasses/Fundraising.java
@@ -11,31 +11,38 @@ import java.util.List;
 @Entity
 public class Fundraising {
     @Id
-    private long fundraisingId;//Для бд
+    private final long fundraisingId;//Для бд
 
-    private double goal;//сумма цели сбора
-    private boolean isGoalAllowed;//есть ли цель
-    private String fundraisingName;//название сбора
-    private Calendar fundraisingExpire;//дата окончания сбора
+    private final double goal;//сумма цели сбора
+    private final boolean isGoalAllowed;//есть ли цель
+    private final String fundraisingName;//название сбора
+    private final Calendar fundraisingExpire;//дата окончания сбора
     @ElementCollection
-    private List<Integer> transactionList;//список транзакций
-    private double currentFundSumm;//текущий сбор
+    private final List<Integer> transactionList;//список транзакций
+    private final double currentFundSum;//текущий сбор
 
-    public long getFundraisingId () {
+    public long getFundraisingId() {
         return fundraisingId;
     }
 
-    public Fundraising(long fundraisingId,double goal,boolean isGoalAllowed,String fundraisingName,Calendar fundraisingExpire,ArrayList<Integer> transactionList,double currentFundSumm) {
+    public Fundraising(long fundraisingId, double goal, boolean isGoalAllowed, String fundraisingName,
+                       Calendar fundraisingExpire, ArrayList<Integer> transactionList, double currentFundSum) {
         this.fundraisingId = fundraisingId;
         this.goal = goal;
         this.isGoalAllowed = isGoalAllowed;
         this.fundraisingName = fundraisingName;
         this.fundraisingExpire = fundraisingExpire;
         this.transactionList = transactionList;
-        this.currentFundSumm = currentFundSumm;
+        this.currentFundSum = currentFundSum;
     }
 
     protected Fundraising() {
-
+        this.fundraisingId = 0;
+        this.goal = 0.;
+        this.isGoalAllowed = false;
+        this.fundraisingName = "fundraisingName";
+        this.fundraisingExpire = Calendar.getInstance();
+        this.transactionList = new ArrayList<>();
+        this.currentFundSum = 0.;
     }
 }
---
+++ springServer/src/test/java/com/example/demo/serverClasses/FundraisingTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.serverClasses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FundraisingTest {
+
+    @Test
+    void getFundraisingId() {
+    }
+}
